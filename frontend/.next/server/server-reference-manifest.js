22134799bd1a4133455dbc16b89fbd278aa9865e
Neda2017 <107911909+Neda2017@users.noreply.github.com>
2025-11-15 11:05:31 +0200

    commit

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"3mGETLD74bkpMdf6X04WRuMgB9zaYX9yAd31tKuc/qU=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"VciQ442mWywoLv4+/SSGVNKn2h5KF+YlMCXAQ4QPxbg=\"}"
